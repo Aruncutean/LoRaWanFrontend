129aa08347e89b52fb00f84f871733a2f592f7ae
Aruncutean <aruncutean.raul12@gmail.com>
2022-02-17 10:38:09 +0200

    D

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+
 app.use(express.static(__dirname + '/dist/front-end'));
 app.get('/*', function(req,res) {
+
+const proxy = require('http-proxy-middleware')
+var apiProxy = proxy('/api', {target: 'https://licenta-db.herokuapp.com/'});
+app.use(apiProxy)
+
 res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));});
 app.listen(process.env.PORT || 8080);
